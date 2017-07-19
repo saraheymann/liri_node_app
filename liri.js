6ad7a9807add113377092a84299322bddc9f9728
sara heymann <saraheymann@saras-mbp.attlocal.net>
2017-07-19 12:31:09 -0500

    finished with do what it says

--- liri.js
+++ liri.js
@@ -154,19 +154,17 @@ function doWhatItSays(){
     fs.readFile("random.txt", "utf8", function(error, data) {
 
   // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return console.log(error);
-  }
+        if (error) {
+            return console.log(error);
+        }
+            //split it by commas, turn it into an array
+            var dataArr = data.split(",");
+            // send the command through the switch and the song title as userInput
+            action = dataArr[0];
+            userInput= dataArr[1];
+            // then spotify the song
+            spotifySong();
 
-  // We will then print the contents of data
-  console.log(data);
-
-  // Then split it by commas (to make it more readable)
-  var dataArr = data.split(",");
-
-  // We will then re-display the content as an array for later use.
-  console.log(dataArr);
-
-});
+    });
 
 }
